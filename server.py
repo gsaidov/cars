5c37c8c0596baf4ee250019741c9f526dc31fdd5
Gulomjon Saidov <gulomjonsaidov@gmail.com>
2019-03-13 09:48:26 -0700

    server.py

---
+++ server.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template, request, jsonify, Response
+import pickle
+
+# create the app objest that will route our calls
+app = Flask(__name__)
+
+@app.route('/', methods = ['GET'])
+def home():
+    return '<p> Hello </p>'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port = 3333, debug=True)
